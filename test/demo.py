cf61150f95df4cd68c75d86619f6ea44f0f49c1e
Mau L Tamez <jose.ledezma@iteso.mx>
2024-10-05 01:10:35 -0600

    Feature(api-enhancement): Add test for credit_pred.csv

--- test/demo.py
+++ test/demo.py
@@ -1,11 +1,17 @@
 '''Demo for API'''
 import requests
 
-data = list(range(23))
 
-response = requests.post('http://127.0.0.1:1234/predict', json=data, timeout = 30)
-print(response)
+def test_api_request(data: list[float]) -> dict[str, int]:
+    response = requests.post('http://127.0.0.1:1234/predict', json=data, timeout = 30)
+    print(response)
 
-prediction = response.json()
+    prediction = response.json()
 
-print(prediction)
+    print(prediction)
+
+    return prediction
+
+if __name__ == "__main__":
+    data = list(range(23))
+    test_api_request(data)
